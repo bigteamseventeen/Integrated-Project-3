7baa9bd3909336c6f3c4e6c662160caad135f486
Thomas <wee.t9mmy99@gmail.com>
2019-04-13 01:51:29 +0100

    Added Chart Rendering onClick
    + Charts will now load when a show is selected
    + Changed chart from LineChart to ScatterChart (will soon give user option to choose)
    - Stopped using vue-google-charts (it's insufficient)
    - Removed data() hardcoded rendering function

--- resources/vue/Pages/TVSeries.vue
+++ resources/vue/Pages/TVSeries.vue
@@ -4,13 +4,7 @@
 			<div class="row">
 				<div class="col-md-8">
 					<h3>TV Series Visualizer</h3>
-					<div>
-						<GChart
-						type="LineChart"
-						:data="chartData"
-						:options="chartOptions"
-						@ready="onChartReady"
-						/>
+					<div id="main-chart">
 					</div>
 				</div>
 
@@ -49,9 +43,12 @@
 	export default class TVSeries extends Vue {
 		popularShows: SeriesList.Result[] = [];
 		self: TVSeries;
-		selectedTVSeries: SeriesList.Result = null;
+
+		selectedTVSeries: SeriesList.Result["id"] = null;
+
 		query: string = "";
 
+		/*
 		data() {
 			return {
 				// Array will be automatically processed with visualization.arrayToDataTable function
@@ -63,7 +60,7 @@
 					["4", 5, 5, 6]
 				],
 				chartOptions: {
-					title: "Company Performance",
+					title: "",
 					subtitle: "Sales, Expenses, and Profit: 2014-2017",
 					height: '500',
 					chartArea: {'width' : '100%', 'height' : '80%', 'left' : '20', },
@@ -71,52 +68,122 @@
 				}
 			};
 		}
-
+		*/
 
 		constructor() {
 			super();
 			this.self = this;
+
 			this.updateShowList();
-			this.data();
-			console.log("here");
-			console.log(this.data());
+
 		 }
-
-		async mounted() {
-			/*
-			this.geolocate();
-
-			let $this = this;
-			GetEarthQuakes(EQDateSpan.Week, EQSignificance.ALL, async function(response,status) {
-				if (status != "success") {
-					console.warn("Failed to recieve information from geological API");
-					return;
-				}
-
-				for (let x=0; x < response.features.length; x++) {
-					let eq: Feature = response.features[x];
-
-					let marker = {
-						lat: eq.geometry['coordinates'][1],
-						lng: eq.geometry['coordinates'][0]
-					};
-
-					$this.markers.push({ position: marker, label: eq.properties.mag.toString(), earthquake: eq });
-				}
-			});
-			*/
-		} // On Component Load, use instead of constructor!
 
 		loadShowInformation_Click(): void {
 			let $this = this;
-
-
 		}
 
-		loadShow(tvid: SeriesList.Result) {
+		loadShow(tvid: SeriesList.Result["id"]) {
+			google.charts.load('current', {
+				packages: ['corechart', 'line']
+			});
 			let $this = this;
-
 			this.selectedTVSeries = tvid;
+
+			// Data collection
+			let showData = [];
+
+			// Get show ID
+			//showID = popularShows["results"][shID]["id"];
+
+			// Get title of show
+			//title = popularShows["results"][shID]["name"];
+
+			GetTVID(tvid, function(info ,status) {
+				if (status != "success") {
+					console.warn("Failed to recieve information from TMDB Api");
+					return;
+				}
+				console.log("response 1: " + (info["seasons"].length-1));
+
+				GetTVID_Season(tvid, (info["seasons"].length-1), function(latestSeason ,status) {
+					if (status != "success") {
+						console.warn("Failed to recieve information from TMDB Api");
+						return;
+					}
+
+					console.log("response 2: " + latestSeason);
+
+					//title = currentShow["name"]
+					let todaysDate = new Date();
+					let title = info["name"];
+					let seasonCount = info["seasons"].length;
+					let hasSpecials = false;
+					hasSpecials = info["seasons"][0]["name"] === "Specials";
+					let lastEpisodeAirDate = new Date(latestSeason["episodes"][latestSeason["episodes"].length - 1]["air_date"]);
+					if (lastEpisodeAirDate > todaysDate) {
+						seasonCount--;
+					}
+
+					let i = hasSpecials ? 1 : 0;
+					let episodeCount = 0;
+
+					for (i; i < seasonCount; i++) {
+						GetTVID_Season(tvid, i, function(response ,status) {
+							if (status != "success") {
+								console.warn("Failed to recieve information from TMDB Api");
+								return;
+							}
+							let seasonIteration = response;
+							for (var j = 0; j < seasonIteration["episodes"].length; j++) {
+								showData.push([(i - (hasSpecials ? 1 : 0)) + ((1 / (seasonIteration["episodes"].length - 1)) * (j))+ 0.5]);
+								var v = hasSpecials ? 1 : 0;
+								for (v; v < i; v++) {
+									showData[episodeCount].push(null);
+									showData[episodeCount].push("S" + i + "E" + (j+1) + " Rating: " + seasonIteration["episodes"][j]["vote_average"])
+								}
+								showData[episodeCount].push(seasonIteration["episodes"][j]["vote_average"]);
+								showData[episodeCount].push("S" + i + "E" + (j+1) + " Rating: " + seasonIteration["episodes"][j]["vote_average"])
+								for (var x = i + 1; x < seasonCount; x++) {
+									showData[episodeCount].push(null);
+									showData[episodeCount].push("S" + i + "E" + (j+1) + " Rating: " + seasonIteration["episodes"][j]["vote_average"])
+								}
+								episodeCount++;
+							}
+						});
+						console.log(i);
+					}
+					google.setOnLoadCallback(function() {
+						draw(hasSpecials, seasonCount, showData, title);
+					});
+				});
+			});
+
+			// tv/popular?page=1&language=en-US
+			// tv/{tv_id}/season/{season_number}&language=en-US
+
+			function draw(hasSpecials, seasonCount, showData, title) {
+				let dataTbl = new google.visualization.DataTable();
+				dataTbl.addColumn('number', 'Season');
+				let i = hasSpecials ? 1 : 0;
+				for (i; i < seasonCount; i++) {
+					dataTbl.addColumn('number', 'Season ' + i);
+					dataTbl.addColumn({'type': 'string', 'role': 'tooltip' });
+				}
+				dataTbl.addRows(showData);
+				let options = {
+					title: title,
+					height: 500,
+					chartArea: {'width' : '100%', 'height' : '80%', 'left' : '40', },
+					legend : {'position': 'bottom'},
+					seriesType : 'scatter',
+					//series: {8: {type: 'line'}},
+				};
+
+				let chart = new google.visualization.ComboChart(document.getElementById('main-chart'));
+				console.log(showData);
+				console.log(dataTbl);
+				chart.draw(dataTbl, options);
+			}
 		}
 
 		updateShowList() {
@@ -161,6 +228,7 @@
 		*/
 	};
 </script>
+
 
 <style scoped>
 	.container-map {
